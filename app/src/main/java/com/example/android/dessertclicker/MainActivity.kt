9d48107307f87f09c4bd1b53f463fa0ea562702c
Ghifari <ghifarialqodri673@gmail.com>
2021-03-24 17:14:57 +0700

    Mengatasi Perubahan Konfigurasi

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -27,6 +28,9 @@ import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
+
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
 
 class MainActivity : AppCompatActivity() {
 
@@ -67,6 +71,13 @@ class MainActivity : AppCompatActivity() {
         }
         dessertTimer = DessertTimer(this.lifecycle)
 
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            showCurrentDessert()
+        }
+
+
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -75,33 +86,43 @@ class MainActivity : AppCompatActivity() {
         binding.dessertButton.setImageResource(currentDessert.imageId)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+    }
+
     override fun onStart() {
         super.onStart()
         Timber.i("onStart Called")
 
     }
+
     override fun onResume() {
         super.onResume()
         Timber.i("onResume Called")
     }
+
     override fun onPause() {
         super.onPause()
         Timber.i("onPause Called")
     }
+
     override fun onStop() {
         super.onStop()
         Timber.i("onStop Called")
 
     }
+
     override fun onDestroy() {
         super.onDestroy()
         Timber.i("onDestroy Called")
     }
+
     override fun onRestart() {
         super.onRestart()
         Timber.i("onRestart Called")
     }
-
 
 
     /**
